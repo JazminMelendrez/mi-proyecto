a89633662914145f7843185631fe8713ce7a928d
Jazmin Melendrez <jazmin.melendrez@alumnos.udg.mx>
2016-12-08 17:54:23 -0600

    Creacion de la clase Imagen, Sonido y Color

--- martes.py
+++ martes.py
@@ -1,1 +1,22 @@
 #empezando con git #
+class Imagen():
+    def tamarindo(self):
+        print("Tamarindo 1,2,3")
+    def escala_grises(self):
+        pass
+    def binarizacion(self):
+        pass
+class Sonido():
+    def reproducir(self):
+        print("Reproduciendo...")
+    def volumne(self):
+        pass
+    def fourier(self):
+        pass
+class Color():
+    def rojo(self):
+        pass
+    def verde(self):
+        pass
+    def azul(self):
+        pass
