13f106cb8f228bab5a48fdef8c321ed44794debf
Jazmin Melendrez <jazmin.melendrez@alumnos.udg.mx>
2016-12-08 15:07:56 -0600

    inicio de  proyecto

--- martes.py
+++ martes.py
@@ -1,15 +1,1 @@
-#empezando con git#
-class Imagen():
-    def tamarindo(self):
-        print("1,2,3...tamarindo")
-    def escala_grises(self):
-        pass
-    def binarizacion (self):
-        pass
-class sonido():
-    def reproducir(self):
-        print("reproduciendo...")
-    def volumen(self):
-        pass
-    def fourier(self):
-        pass
+#empezando con git #
